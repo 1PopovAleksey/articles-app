b5b9354e7e9ce2bd429b5adad1a6634ddddc907a
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-02 10:34:53 +0700

    Added PostList.vue

---
+++ src/components/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)"/>
+  </div>
+  <h2 v-else>
+    Список постов пуст
+  </h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 30px 0 0 0;
+}
+
+h2 {
+  margin: 30px 0 0 0;
+  color: #c81414;
+}
+</style>
