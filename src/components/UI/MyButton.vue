a861f2597a25b6a44d7f5b039b708108094799df
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-02 10:33:44 +0700

    Added MyButton.vue

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="del-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'my-button',
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 10px;
+  border: none;
+  background: #121212;
+  color: #FFF;
+  font-family: Monospace, system-ui;
+}
+</style>
