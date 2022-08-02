9810df9d6c5af47bac0d6cd3361b9a8b56b14c44
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-02 10:34:35 +0700

    Added PostForm.vue

---
+++ src/components/PostForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Создание поста</h2>
+    <form @submit.prevent>
+      <my-input v-model="post.title" class="input" type="text"
+             placeholder="Название"/>
+      <my-input v-model="post.descr" class="input" type="text"
+             placeholder="Описание"/>
+      <my-button type="submit" @click="createPost">Создать</my-button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        descr: '',
+      }
+    }
+  },
+  methods: {
+    createPost(event) {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        descr: '',
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
