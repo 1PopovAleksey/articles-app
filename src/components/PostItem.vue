5780b82dd1ddae39a77a52040986f7d9e9972b74
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-03 13:59:50 +0700

    Refactoring PostItem.vue

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="post">
-    <div>
+    <div class="posts">
       <div><p><strong>Название:</strong> {{ post.title }}</p></div>
       <div><p><strong>Описание:</strong> {{ post.body }}</p></div>
     </div>
     <div class="post__btn">
       <my-button class="del-btn" @click="$emit('remove', post)">X</my-button>
+      <my-button class="open-btn" @click="$router.push(`/posts/${post.id}`)">O</my-button>
     </div>
   </div>
 </template>
@@ -22,15 +23,26 @@ export default {
 </script>
 
 <style scoped>
+.posts {
+  margin: 0 20px 0 0;
+}
+
 .post {
   display: flex;
   justify-content: space-between;
   padding: 15px;
+  width: 800px;
   box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.2);
 }
 
 .post:not(:first-child) {
   margin: 20px 0 0 0;
+}
+
+.post__btn {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
 }
 
 .del-btn {
@@ -41,5 +53,16 @@ export default {
 
 .del-btn:hover {
   color: #c81414 !important;
+}
+
+.open-btn {
+  margin: 5px 0 0 0;
+  padding: 0 0 0 0.5px;
+  width: 20px;
+  height: 20px;
+}
+
+.open-btn:hover {
+  color: #1414c8 !important;
 }
 </style>
